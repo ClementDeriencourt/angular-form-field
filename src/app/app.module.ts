9ba1fd092d7cae6c2fee590ca5de2d6981c7ecb4
Clément Deriencourt <clement-deriencourt@orange.fr>
2020-09-23 15:54:33 +0200

    Material Input fonctionnel avec différents formats ( fill / standard / legacy / outline )

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatFormField, MatFormFieldControl, MatFormFieldModule, MatLabel} from '@angular/material/form-field';
+import {MatOptionModule} from '@angular/material/core';
+import {MatInput, MatInputModule} from '@angular/material/input';
+import {MatInputHarness} from '@angular/material/input/testing';
+import {MatSelectModule} from '@angular/material/select';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent,
+
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatInputModule,
+
+
+
   ],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA,
+  ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
